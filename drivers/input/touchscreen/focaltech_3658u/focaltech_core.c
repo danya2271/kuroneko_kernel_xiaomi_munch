d5bc4d240908497364671d8d6c51e3bbdf066d73
NotZeetaa <rodrigo2005contente@gmail.com>
2023-04-09 19:13:45 +0100

    treewide: Nuke irq boost and lpm_disable
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/input/touchscreen/focaltech_3658u/focaltech_core.c
+++ drivers/input/touchscreen/focaltech_3658u/focaltech_core.c
@@ -73,10 +73,6 @@ struct fts_ts_data *fts_data;
 static int fts_ts_suspend(struct device *dev);
 static int fts_ts_resume(struct device *dev);
 
-#define LPM_EVENT_INPUT 0x1
-extern void lpm_disable_for_dev(bool on, char event_dev);
-extern void touch_irq_boost(void);
-
 #ifdef CONFIG_TOUCHSCREEN_XIAOMI_TOUCHFEATURE
 static void fts_read_palm_data(u8 reg_value);
 static int fts_palm_sensor_cmd(int value);
@@ -418,7 +414,6 @@ void fts_release_all_finger(void)
 #endif
 	input_report_key(input_dev, BTN_TOUCH, 0);
 	input_sync(input_dev);
-	lpm_disable_for_dev(false, LPM_EVENT_INPUT);
 
 	fts_data->touchs = 0;
 	fts_data->key_state = 0;
@@ -526,7 +521,6 @@ static int fts_input_report_b(struct fts_ts_data *data)
 		/* touchs==0, there's no point but key */
 		if (EVENT_NO_DOWN(data) || (!touchs)) {
 			input_report_key(data->input_dev, BTN_TOUCH, 0);
-			lpm_disable_for_dev(false, LPM_EVENT_INPUT);
 		} else {
 			input_report_key(data->input_dev, BTN_TOUCH, 1);
 		}
@@ -742,7 +736,6 @@ static irqreturn_t fts_irq_handler(int irq, void *data)
 #endif
 
 	pm_stay_awake(fts_data->dev);
-	lpm_disable_for_dev(true, LPM_EVENT_INPUT);
 	fts_irq_read_report();
 	pm_relax(fts_data->dev);
 	return IRQ_HANDLED;
