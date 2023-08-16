daf010540cf371161933c00d2410e023336da644
danya2271 <danya2271@yandex.ru>
2023-08-16 15:08:05 +0500

    thaw_fingerprintd: use xiaomi fingerprint service

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -198,7 +198,7 @@ void thaw_fingerprintd(void)
 
 	read_lock(&tasklist_lock);
 	for_each_process(p) {
-	        if (!strstr(p->comm, "erprint")) {
+	        if (!strstr(p->comm, "android.hardware.biometrics.fingerprint@2.3-service.xiaomi", 13)) {
 			__thaw_task(p);
 			break;
 		}
